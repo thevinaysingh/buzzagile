54e5a3929284c80d0be80a4225aba14f4803e8fc
Pushpendra mishra <pushpendra.mishra660@gmail.com>
2019-01-12 15:44:36 +0530

    Added images on constant

--- BuzzAgile/src/assets/images/index.js
+++ BuzzAgile/src/assets/images/index.js
@@ -1,8 +1,12 @@
 /* eslint import/no-unresolved: 0 */
 const splash = require('./splash.png');
 const headerBg = require('./header-bg.png');
+const roundedborder = require('./Path_1649.png');
+const logoImage = require('./logoImage.png');
 
 export const images = {
   splash,
   headerBg,
+  roundedborder,
+  logoImage,
 };
