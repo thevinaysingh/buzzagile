531ed5e2f3a52495c05433f4a30bb6a4bf2f2b27
Pushpendra mishra <pushpendra.mishra660@gmail.com>
2019-01-14 21:04:55 +0530

    Create custom input component

---
+++ BuzzAgile/src/components/CustomInput.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  TextInput,
+  StyleSheet,
+  ImageBackground,
+} from 'react-native';
+import { images } from '../assets/images';
+
+const styles = StyleSheet.create({
+  buttonStyle: {
+    justifyContent: 'center',
+    alignSelf: 'center',
+    fontSize: 18,
+    marginTop: 0,
+    height: 40,
+    color: '#0000ff',
+  },
+  RoundedbuttonStyle: {
+    width: '70%',
+    height: 80,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+const CustomInput = props => (
+  <ImageBackground
+    source={ images.inputField }
+    style={ styles.RoundedbuttonStyle }
+  >
+    <TextInput
+      style={{ height: 50, fontSize: 18, bottom: 5 }}
+      placeholder={ props.placeholder }
+      value={props.text}
+      onChangeText={(text) => this.setState({text})}
+      onChangeText={(text) => props.onChange(text)}
+    />
+  </ImageBackground>
+);
+
+CustomInput.propTypes = {
+  title: PropTypes.string,
+  placeholder: PropTypes.string,
+};
+
+CustomInput.defaultProps = {
+  title: 'hello world',
+  placeholder: 'Placeholder text',
+};
+
+export default CustomInput;
