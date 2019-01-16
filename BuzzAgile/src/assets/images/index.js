2dc2ff5627fd1e14611ba2f1d7f551ba4689e182
Pushpendra mishra <pushpendra.mishra660@gmail.com>
2019-01-16 22:40:02 +0530

    Add require images

--- BuzzAgile/src/assets/images/index.js
+++ BuzzAgile/src/assets/images/index.js
@@ -3,10 +3,22 @@ const splash = require('./splash.png');
 const headerBg = require('./header-bg.png');
 const roundedborder = require('./Path_1649.png');
 const logoImage = require('./logoImage.png');
+const menuImage = require('./menu.png');
+const notificationBell = require('./menu.png');
+const reward = require('./reward.png');
+const prize = require('./prize.png');
+const rank = require('./rank.png');
+const yelloRupeeButtom = require('./yellow_rupee.png');
 
 export const images = {
   splash,
   headerBg,
   roundedborder,
   logoImage,
+  menuImage,
+  notificationBell,
+  reward,
+  prize,
+  rank,
+  yelloRupeeButtom,
 };
