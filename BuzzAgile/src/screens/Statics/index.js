94d627ab2166a06bccfc10752677666cfc7f2d5d
Pushpendra mishra <pushpendra.mishra660@gmail.com>
2019-02-03 20:38:13 +0530

    Add static screen

---
+++ BuzzAgile/src/screens/Statics/index.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { View } from 'react-native';
+import { RootContainer } from 'components/AppStyledComponents';
+import { screens } from 'navigator/screens-name';
+import RoundedButtonFilled from 'components/RoundedButtonFilled';
+import Logo from 'components/Logo';
+import CustomText from 'components/CustomText';
+import { images } from 'assets/images';
+
+/**
+  * @desc this class will display the options to redirect to login or signup
+  * @author Pushpendra mishra pushpendra.mishra660@gmail.com
+  * @required null
+*/
+
+class Welcome extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  onPressGetStarted() {
+    this.props.navigation.navigate(screens.ENTER_PHONE);
+  }
+
+  onPressLogin() {
+    this.props.navigation.navigate(screens.LOGIN);
+  }
+
+  render() {
+    return (
+      <RootContainer>
+        <View style={{ flex: 2, alignItems: 'center' }}>
+          <Logo />
+        </View>
+      </RootContainer>
+    );
+  }
+}
+
+Welcome.propTypes = {
+  navigation: PropTypes.objectOf(PropTypes.any).isRequired,
+};
+
+export default Welcome;
