1bbea1e3aed09653c623134f4faf3e989c8408f1
Pushpendra mishra <pushpendra.mishra660@gmail.com>
2019-02-10 15:16:11 +0530

    Add select language popup and flow managment

--- BuzzAgile/src/screens/SelectLanguage/index.js
+++ BuzzAgile/src/screens/SelectLanguage/index.js
@@ -1,22 +1,104 @@
 import React, { Component } from 'react';
+import { View, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
 import PropTypes from 'prop-types';
+import { StackActions, NavigationActions } from 'react-navigation';
 import RoundedButton from 'components/RoundedButton';
 import Logo from 'components/Logo';
-import { RootContainer, FlexContainerCenteredChild } from 'components/AppStyledComponents';
+import { DefaultLabel, RootContainer, FlexContainerCenteredChild } from 'components/AppStyledComponents';
 import { screens } from 'navigator/screens-name';
+import { Dimens } from 'theme';
+import * as Storage from 'utils/storage';
+import { CONSTANTS } from 'utils/constants';
+
+const languages = [
+  {
+    label: 'English',
+    value: 'en',
+  },
+];
+
+const styles = StyleSheet.create({
+  popupContainer: {
+    width: Dimens.getViewportWidth(),
+    height: Dimens.getViewportHeight(),
+    position: 'absolute',
+    backgroundColor: '#00000050',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  popupBody: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  listContainer: {
+    height: Dimens.getViewportHeight() / 3,
+    width: Dimens.getViewportWidth() - 100,
+    borderRadius: 10,
+    backgroundColor: 'white',
+    padding: 20,
+    alignSelf: 'center',
+  },
+  text: {
+    color: 'darkgrey',
+    marginVertical: 40,
+  },
+});
 
 class SelectLanguage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedLaguage: 'English',
+      language: '',
+      showPopup: false,
     };
   }
 
-  onSelectLanguage() {
-    // TODO: Implement select language functionality.
-    console.log('selectedLaguage', this.state.selectedLaguage);
-    this.props.navigation.navigate(screens.WELCOME);
+  onSelectLanguage(lang, index) {
+    console.log('Selected Language : ', languages[index]);
+    this.setState({
+      language: lang.label,
+      showPopup: false,
+    });
+
+    Storage.setItemWithKeyAndValue(CONSTANTS.KEYS.APP_LANGUAGE, lang.value);
+    Storage.setItemWithKeyAndValue(CONSTANTS.KEYS.APP_NOT_OPEN_FIRST_TIME, true);
+    const resetAction = StackActions.reset({
+      index: 0,
+      key: null,
+      actions: [
+        NavigationActions.navigate({ routeName: screens.WELCOME }),
+      ],
+    });
+    this.props.navigation.dispatch(resetAction);
+  }
+
+  renderSelectLangPopup() {
+    return (
+      <TouchableOpacity
+        style={ styles.popupContainer }
+        onPress={ () => this.setState({ showPopup: false }) }
+      >
+        <ScrollView contentContainerStyle={ styles.popupBody }>
+          <View style={ styles.listContainer }>
+            <DefaultLabel style={{ fontWeight: '600', fontSize: 18 }}>
+              Choose one option
+            </DefaultLabel>
+            {
+              languages.map((l, i) => (
+                <TouchableOpacity
+                  key={ `lang${i}` }
+                  style={{ padding: 15 }}
+                  onPress={ () => this.onSelectLanguage(l, i) }
+                >
+                  <DefaultLabel> { l.label }</DefaultLabel>
+                </TouchableOpacity>
+              ))
+            }
+          </View>
+        </ScrollView>
+      </TouchableOpacity>
+    );
   }
 
   render() {
@@ -29,10 +111,11 @@ class SelectLanguage extends Component {
           style={{ justifyContent: 'flex-start' }}
         >
           <RoundedButton
-            title="Select Language"
-            onPress={ () => this.onSelectLanguage() }
+            title={ this.state.language === '' ? 'Select Language' : this.state.language }
+            onPress={ () => this.setState({ showPopup: true }) }
           />
         </FlexContainerCenteredChild>
+        { this.state.showPopup && this.renderSelectLangPopup() }
       </RootContainer>
     );
   }
